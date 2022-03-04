54d1a6adc325ef92a6545187a2a680769202d34a
Bedran Piro <Bedranmk@gmail.com>
2022-03-04 16:25:04 +0300

    Commit

--- src/test/java/tests/CartTests.java
+++ src/test/java/tests/CartTests.java
@@ -3,27 +3,26 @@ package tests;
 import org.testng.annotations.Test;
 import pages.CartPageObject;
 public class CartTests extends LoginTests{
+
     @Test
-    public void addCartTest() throws InterruptedException {
+    public void addCartTest(){
         LoginTest();
         CartPageObject cartPageObject = new CartPageObject(driver);
         cartPageObject.addBackpack();
         cartPageObject.addFleeceJacket();
-        Thread.sleep(2000);
         cartPageObject.shoppingCartContainer();
-        Thread.sleep(2000);
     }
     @Test
-    public void removeCartTest() throws InterruptedException {
+    public void removeCartTest(){
         CartPageObject cartPageObject = new CartPageObject(driver);
-        Thread.sleep(3000);
         cartPageObject.removeBackpack();
     }
-    @Test
+
+   /* @Test
     public void ContinueShopping() throws InterruptedException {
         addCartTest();
         CartPageObject cartPageObject = new CartPageObject(driver);
         cartPageObject.setContinue_shopping();
-    }
+    }*/
 
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,20 +1,25 @@
 package tests;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.LoginPageObjects;
 
 public class LoginTests extends BaseTests{
+    //
+    //Positive Test
+    //
     @Test
     public void LoginTest(){
+        setUpTest();
         LoginPageObjects loginPageObjects = new LoginPageObjects(driver);
         loginPageObjects.setTextUserName("standard_user");
         loginPageObjects.setTextPassword("secret_sauce");
         loginPageObjects.ClickLoginButton();
     }
 
+    //
+    //Negative Test
+    //
     @Test
     public void LoginTestNegative(){
         setUpTest();
